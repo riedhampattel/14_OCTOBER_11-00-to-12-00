51b7c4b1eebb82140797a8c62336fb7c03e77371
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-11 12:23:31 +0530

    Add files via upload

--- C Programming/Loop/4.c
+++ C Programming/Loop/4.c
@@ -1,21 +1,25 @@
-//Write a c program to take a number from the user and display it's factorial.
+//Write a c program to take one input from the user and check whether the number is prime or not?
 #include<stdio.h>
 int main()
 {
-	int num,i,fact=1;
-	printf("\nEnter any number to find factorial = ");
+	int num,count=0,i;
+	printf("\nEnter the number to check whether it is prime or not = ");
 	scanf("%d",&num);
 	
-//	for(i=num;i>=1;i--)
-//	{
-//		fact = fact * i;
-//	}
-
-	for(i=1;i<=num;i++)
+	for(i=2;i<num;i++)
 	{
-		fact = fact * i;
+		if(num%i==0)
+		{
+			count++;
+		}
 	}
-	
-	printf("\nFactorial of %d is %d",num,fact);
+	if(count==0)
+	{
+		printf("\n%d is a prime number",num);
+	}
+	else
+	{
+		printf("\n%d is not a prime number",num);
+	}
 	return 0;
 }

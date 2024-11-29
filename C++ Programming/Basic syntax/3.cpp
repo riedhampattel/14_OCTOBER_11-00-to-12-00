c9f0c68e44c667e85a78e88b1669be35650a002f
riedhampattel <riedhampattel.tops@gmail.com>
2024-11-29 11:46:22 +0530

    updated

---
+++ C++ Programming/Basic syntax/3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num = 10;
+	char ch = 'A';
+	double dl = 45.89;
+	string s = "Hello";
+	bool b1 = true;
+	bool b2 = false;
+	
+	cout<<"The value of int = "<<num;
+	cout<<"\nThe value of char = "<<ch;
+	cout<<"\nThe value of double = "<<dl;
+	cout<<"\nThe value of string = "<<s;
+	cout<<"\nThe value of bool = "<<b1;
+	cout<<"\nThe value of bool = "<<b2;
+	return 0;
+}
---
+++ C++ Programming/Basic syntax/4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	char ch;
+	double dl;
+	string s;
+	
+	cout<<"\nEnter the value of num = ";
+	cin>>num;
+	cout<<"\nEnter the value of char = ";
+	cin>>ch;
+	cout<<"\nEnter the value of double = ";
+	cin>>dl;
+	cout<<"\nEnter the value of string = ";
+	cin>>s;
+	
+	cout<<"The value of int = "<<num;
+	cout<<"\nThe value of char = "<<ch;
+	cout<<"\nThe value of double = "<<dl;
+	cout<<"\nThe value of string = "<<s;
+	return 0;
+}

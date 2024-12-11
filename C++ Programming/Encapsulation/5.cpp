048feead37de9aa5efb54e03bc44a793ff455532
riedhampattel <riedhampattel.tops@gmail.com>
2024-12-11 11:58:30 +0530

    updated

---
+++ C++ Programming/Encapsulation/5.cpp
@@ -0,0 +1,101 @@
+//Write a C++ program to implement a class called Date that has private member variables for day, month, and year. Include member functions to set and get these variables, as well as to validate if the date is valid.
+#include<iostream>
+using namespace std;
+class Date
+{
+	private :
+		int day,year;
+		string month;
+	public :
+		int count=0;
+		void set(int d,int m,int y)
+		{
+			if(y%4==0 && m==2 && (d>=1 && d<=29))
+			{
+				day = d;
+				month = "February";
+				year = y;
+			}
+			else if(m==2 && (d>=1 && d<=28))
+			{
+				day = d;
+				month = "February";
+				year = y;
+			}
+			else if((m==4 || m==6 || m==9 || m==11) && (d>=1 && d<=30))
+			{
+				day = d;
+				switch(m)
+				{
+					case 4 :
+						month = "April";
+					break;
+					case 6 :
+						month = "June";
+					break;
+					case 9 :
+						month = "september";
+					break;
+					case 11 :
+						month = "November";
+					break;
+				}
+				year = y;
+			}
+			else if((m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12) && (d>=1 && d<=31))
+			{
+				day = d;
+				switch(m)
+				{
+					case 1:
+						month = "January";
+					break;
+					case 3:
+						month = "March";
+					break;
+					case 5:
+						month = "May";
+					break;
+					case 7:
+						month = "July";
+					break;
+					case 8:
+						month = "August";
+					break;
+					case 10:
+						month = "October";
+					break;
+					case 12:
+						month = "December";
+					break;
+				}
+				year = y;
+			}
+			else
+			{
+				count++;
+			}
+		}
+		void get()
+		{
+			if(count==0)
+			{
+				cout<<"\n"<<day<<" - "<<month<<" - "<<year<<endl;
+			}
+			else
+			{
+				cout<<"\nInvalid date format";
+			}
+		}
+};
+int main()
+{
+	Date d1;
+	d1.set(12,5,2004);
+	Date d2;
+	d2.set(29,2,2004);
+	
+	d1.get();
+	d2.get();
+	return 0;
+}

a0ba4d9b626980027404b2dbd72050fef0bfc872
riedhampattel <riedhampattel.tops@gmail.com>
2024-12-31 12:05:48 +0530

    updated

--- DBMS/1.sql
+++ DBMS/1.sql
@@ -11,3 +11,39 @@ commission_pct float,
 manager_id int,
 department_id int not null
 );
+
+describe employees;
+
+-- modify column
+alter table employees modify hire_date date not null;
+describe employees;
+alter table employees modify department_id int;
+describe employees;
+
+-- rename table
+alter table employees rename to emp_data;
+describe employees;
+describe emp_data;
+alter table emp_data rename to employees;
+
+-- rename column
+alter table employees rename column salary to income;
+describe employees;
+
+-- add column to the end
+alter table employees add column job_id int not null;
+describe employees;
+
+-- add column at specific position
+alter table employees add column job_time time not null after hire_date;
+describe employees;
+
+-- delete column
+alter table employees drop column job_id;
+alter table employees drop column job_time;
+describe employees;
+
+drop table employees;
+
+drop schema 15_october;
+describe employees;
---
+++ DBMS/2.sql
@@ -0,0 +1,23 @@
+insert into employees values (100,'Steven','King','2024-12-25',24000,null,null,90);
+
+select employee_id,first_name from employees;
+
+select first_name,employee_id from employees;
+
+select * from employees;
+
+insert into employees values (103,'Steven','King','2024-12-25',24000,null,null,90);
+
+update employees set department_id = 100;
+
+update employees set department_id = 90 where employee_id = 100;
+update employees set department_id = 90 where employee_id = 101;
+
+update employees set salary = salary+10000 where department_id = 90;
+select * from employees;
+
+delete from employees where employee_id = 100;
+select * from employees;
+
+delete from employees where last_name = 'King';
+select * from employees;

77999922136f84641a2373444bf34b21c4fb6c52
riedhampattel <riedhampattel.tops@gmail.com>
2025-01-03 11:38:01 +0530

    practice

--- DBMS/3.sql
+++ DBMS/3.sql
@@ -47,3 +47,29 @@ select * from employees where department_id not in (90,60,50);
 select * from employees where (manager_id,department_id) in ((100,90),(103,60));
 
 select * from employees where (manager_id,department_id) not in ((124,50),(103,60));
+
+select min(salary) from employees;
+
+select max(salary) from employees;
+
+select sum(salary) from employees;
+
+select avg(salary) from employees;
+
+select count(commission_pct) from employees;
+
+select count(manager_id) from employees;
+
+select count(*) from employees;
+
+select department_id,min(salary) from employees group by department_id order by 2;
+
+select * from employees;
+
+select department_id,min(salary) from employees group by department_id having min(salary)>15000;
+
+select department_id,max(salary) from employees group by department_id having max(salary)>15000;
+
+select * from employees order by salary desc limit 5;
+
+select * from employees order by salary desc limit 10 offset 5;
